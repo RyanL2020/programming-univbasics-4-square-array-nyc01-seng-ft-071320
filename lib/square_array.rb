c5d4cfa203d5e361f55f6677bece00be721463ca
Ryan Locascio <loc.fitness1@gmail.com>
2020-07-02 23:39:35 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,5 +1,10 @@
-require 'pry'
-
 def square_array(array)
-   binding.pry 
+ result = Array.new(array.size)
+  i = 0
+   while i < array.size
+    result[i] = array[i] ** 2
+    i += 1
+  end
+  result
+end
 end
